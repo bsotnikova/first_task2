8eaa23ee944f94c77948b8cd71e60b0cb3f81b2c
be.sotnikova <bsotnikova@mail.ru>
2022-04-12 16:41:28 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        int check = 10;
+        int enrollment = 1090;
+        int bonus = enrollment / 100;
+        int total = check + enrollment + bonus;
+
+        System.out.println(total);
+    }
+}
