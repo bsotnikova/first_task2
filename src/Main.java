fd980ef932fb56fcfe48680bf58c43939a16b6cb
be.sotnikova <bsotnikova@mail.ru>
2022-09-01 22:10:37 +0400

    add condition

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args){
-        int check = 10;
-        int enrollment = 1090;
-        int bonus = enrollment / 100;
-        int total = check + enrollment + bonus;
-
+        int check = 100;
+        int enrollment = 1100;
+        int total=0;
+        int bonus = 0;
+        if (enrollment >= 1000){
+            bonus = enrollment / 100;
+        }
+        total = check + enrollment + bonus;
         System.out.println(total);
     }
 }
